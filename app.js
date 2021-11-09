db8555f4b3b52896545ae7f7146b766498dc9de8
Akshay Arora <akshayxarora@gmail.com>
2021-11-09 21:56:21 +0530

    Made changes in the Code

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
-let quoteDisplay = document.getElementById("quote")
+const quoteDisplay = document.getElementById("quotes");
+let quoteArray = [];
+
+let ctr = 0;
+let ctrLength = 10;
+
+fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    quoteArray = data;
+    printText(quoteArray);
+})
+
+function printText(quotes){
+    for(;ctr < ctrLength; ctr++){
+        quoteDisplay.innerHTML += `
+        <blockquote class="blockquote shadow text-center" id="quote">
+        <p class="mb-0">${quotes[ctr].text}</p>
+        <footer class="blockquote-footer mt-2">${quotes[ctr].author}</footer>
+      </blockquote>`
+    }
+    ctrLength += ctr;
+    console.log(ctrLength);
+}
 
 
