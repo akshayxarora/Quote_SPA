e8e9f5f85814e2d85d9ef2df905743e8a9801659
Akshay Arora <akshayxarora@gmail.com>
2021-11-14 03:07:34 +0530

    Changes in UI

--- app.js
+++ app.js
@@ -6,26 +6,26 @@ let ctrLength = 10;
 
 
 fetch("https://type.fit/api/quotes")
-  .then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(data) {
-      quoteArray = data;
+  .then(function (data) {
+    quoteArray = data;
     printText(quoteArray);
-}).then(
-    document.getElementById('add-quotes').addEventListener('click',  function() { printText(quoteArray) })
-)
+  }).then(
+    document.getElementById('add-quotes').addEventListener('click',function(){ printText(quoteArray) })
+  )
 
 
-function printText(quotes){
-    for(;ctr < ctrLength; ctr++){
-        quoteDisplay.innerHTML += `
+function printText(quotes) {
+  for (; ctr < ctrLength; ctr++) {
+    quoteDisplay.innerHTML += `
         <blockquote class="blockquote shadow text-center" id="quote">
         <p class="mb-0">${quotes[ctr].text}</p>
         <footer class="blockquote-footer mt-2">${quotes[ctr].author}</footer>
       </blockquote>`;
-    }
-    ctrLength += ctr;
+  }
+  ctrLength += ctr;
 };
 
 
